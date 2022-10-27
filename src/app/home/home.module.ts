a28c8ee4b13155012f3206034bc2b5d262900c66
Rubens Rocha <rubensrocha50@gmail.com>
2022-10-27 18:10:02 -0300

    API, Autenticação e Cadastro

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,19 +1,25 @@
+import { MensagemModule } from './../componentes/mensagem/mensagem.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
+import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    NovoUsuarioComponent
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    FormsModule,
+    MensagemModule
   ],
   exports:[
     HomeComponent
